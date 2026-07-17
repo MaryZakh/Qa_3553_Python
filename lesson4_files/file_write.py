66734d0f26f17ae692e1dc2f79550f4146222be5
Mariia Zakharevich <z0559882272@gmail.com>
2026-07-17 12:00:45 +0300

    added Files

---
+++ lesson4_files/file_write.py
@@ -0,0 +1,20 @@
+with open("greeting.txt","w",encoding="utf-8") as file:
+    file.write("Hello, automated tests!\n")
+    file.write("Second line")
+
+#file = open(......)
+#file.close()
+
+# "r" - read
+# "w" - write
+# "a" - append
+# "r+" - read & write
+# "rb", "wb" - pdf, screenshots
+
+def log_test_result(test_name, status):
+    with open("test_run.log","a",encoding="utf-8") as log_file:
+        log_file.write(f"{test_name}:{status}\n")
+
+log_test_result("test_login","PASSED")
+log_test_result("test_registration","FAILED")
+log_test_result("test_logout","PASSED")
